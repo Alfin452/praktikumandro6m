686ad98b1c3ca8565f09394ad510569b002e16a9
Nosezzy <151822668+Alfin452@users.noreply.github.com>
2025-06-28 11:03:05 +0800

    PERTEMUAN 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
+import 'agenda_list.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Muhammad Alfin Nur Huda',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: Center(child: Text('Halo Selamat Datang di Flutter')),
-      ),
+      title: 'Agenda App',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const AgendaList(),
     );
   }
 }
